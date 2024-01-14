e85e9bc435d0dfc498c12c9ff572753630e1a386
Takayuki Takahashi <ttkaws15@gmail.com>
2024-01-15 08:24:51 +0900

    pokemonapiからデータを取得

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async() => {
+      // すべてのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>pokemon</h1>
     </div>
   );
 }
