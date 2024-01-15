52d20221fec5538f11ef86a1869fc45703a97650
Takayuki Takahashi <ttkaws15@gmail.com>
2024-01-16 07:59:42 +0900

    loadが終わったかどうかで記述を分岐 useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async() => {
       // すべてのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
+
   },[]);
   return (
     <div className="App">
+      {loading ? (
+        <h1>now loading</h1>
+      ) : (
+        <h1>load completed</h1>
+      )}
       <h1>pokemon</h1>
     </div>
   );
